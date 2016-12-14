9c8585980c09df47e6e4220bb79cb3085c5a3d4c
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-14 11:16:55 -0800

    Setup an initial form to capture faculty term information

--- includes/class-graduate-degree-faculty-taxonomy.php
+++ includes/class-graduate-degree-faculty-taxonomy.php
@@ -36,6 +36,7 @@ class WSUWP_Graduate_Degree_Faculty_Taxonomy {
 	 */
 	public function setup_hooks() {
 		add_action( 'init', array( $this, 'register_taxonomy' ), 20 );
+		add_action( "{$this->taxonomy_slug}_edit_form_fields", array( $this, 'term_edit_form_fields' ), 10 );
 	}
 
 	/**
@@ -66,5 +67,71 @@ class WSUWP_Graduate_Degree_Faculty_Taxonomy {
 			'rewrite'           => false,
 		);
 		register_taxonomy( $this->taxonomy_slug, array( WSUWP_Graduate_Degree_Programs()->post_type_slug ), $args );
+	}
+
+	/**
+	 * Captures information about a faculty member as term meta.
+	 *
+	 * @since 0.0.1
+	 *
+	 * @param WP_Term $term
+	 */
+	public function term_edit_form_fields( $term ) {
+		?>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="degree-abbreviation">Degree abbreviation</label>
+			</th>
+			<td>
+				<input type="text" name="degree_abbreviation" id="degree-abbreviation" value="" />
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="degree-abbreviation">Email</label>
+			</th>
+			<td>
+				<input type="text" name="email" id="email" value="" />
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="may-chair">May chair committee</label>
+			</th>
+			<td>
+				<select name="may_chair" id="may-chair">
+					<option value="yes">Yes</option>
+					<option value="no">No</option>
+				</select>
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="may-cochair">May cochair committee</label>
+			</th>
+			<td>
+				<select name="may_cochair" id="may-cochair">
+					<option value="yes">Yes</option>
+					<option value="no">No</option>
+				</select>
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="teaching-interests">Teaching interests</label>
+			</th>
+			<td>
+				<textarea name="teaching_interests" id="teaching-interests" rows="5" cols="50" class="large-text"></textarea>
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="research-interests">Research interests</label>
+			</th>
+			<td>
+				<textarea name="research_interests" id="research-interests" rows="5" cols="50" class="large-text"></textarea>
+			</td>
+		</tr>
+		<?php
 	}
 }
