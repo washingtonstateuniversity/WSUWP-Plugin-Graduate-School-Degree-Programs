76a320660e1047b8a255f9a1f27e4005a6aa684c
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-24 17:20:03 -0800

    Update add button class for consistent naming

--- includes/class-wsuwp-graduate-degree-programs.php
+++ includes/class-wsuwp-graduate-degree-programs.php
@@ -297,7 +297,7 @@ class WSUWP_Graduate_Degree_Programs {
 					?><input type="text" name="<?php echo esc_attr( $key ); ?>[]" value="" /></span><?php
 				}
 
-				echo '<input type="buttom" class="factsheet-add-' . esc_attr( $meta['type'] ) . ' button" value="Add" /></div>';
+				echo '<input type="buttom" class="add-factsheet-' . esc_attr( $meta['type'] ) . '-field button" value="Add" /></div>';
 
 			}
 
